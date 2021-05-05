4ca463b6909d5f0c4b9b3ca3528fab57c09ce39b
k.k <nrdy999902@gmail.com>
2021-05-06 04:14:51 +0900

    Add index jsp

--- src/controllers/personal_data/Personal_dataNewServlet.java
+++ src/controllers/personal_data/Personal_dataNewServlet.java
@@ -1,9 +1,7 @@
 package controllers.personal_data;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,19 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Personal_data;
-import utils.DBUtil;
 
 /**
- * Servlet implementation class Personal_dataIndexServlet
+ * Servlet implementation class Personal_dataNewServlet
  */
-@WebServlet("/personal/index")
-public class Personal_dataIndexServlet extends HttpServlet {
+@WebServlet("/personal/new")
+public class Personal_dataNewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Personal_dataIndexServlet() {
+    public Personal_dataNewServlet() {
         super();
 
     }
@@ -33,15 +30,10 @@ public class Personal_dataIndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("personal_data", new Personal_data());
 
-        List<Personal_data> personal_data = em.createNamedQuery("getAllPersonal_data", Personal_data.class).getResultList();
-
-        em.close();
-
-        request.setAttribute("personal_data", personal_data);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/daily_kcal/index.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/personal_data/new.jsp");
         rd.forward(request, response);
     }
 
